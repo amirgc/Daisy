55115adb236dce06df2c4155035b1562dea08718
amirgc <gcamir15@gmail.com>
2019-01-02 07:22:13 -0600

    api extended

---
+++ DatingApp.API/Data/DatingRepository.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingRepository : IDatingRepository
+    {
+        private readonly DataContext _context;
+        public DatingRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public Task<Photo> GetMainPhotoForUser(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Photo> GetPhoto(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> GetUser(int id, bool isCurrentUser = false)
+        {
+            var query = _context.Users.Include(p => p.Photos).AsQueryable();
+
+            if (isCurrentUser)
+                query = query.IgnoreQueryFilters();
+
+            var user = await query.FirstOrDefaultAsync(u => u.Id == id);
+
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+            return users;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+              return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ DatingApp.API/Data/Seed.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using System.Linq;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        private readonly UserManager<User> _userManager;
+        // private readonly RoleManager<Role> _roleManager;
+        private readonly DataContext _context;
+        public Seed(DataContext context)
+        {
+            _context = context;
+            // UserManager<User> userManager, RoleManager<Role> roleManager
+            // _userManager = userManager;
+            // _roleManager = roleManager;
+        }
+
+        public void SeedUsers()
+        {
+            // if (!_userManager.Users.Any())
+            // {
+                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+                // var roles = new List<Role>
+                // {
+                //     new Role{Name = "Member"},
+                //     new Role{Name = "Admin"},
+                //     new Role{Name = "Moderator"},
+                //     new Role{Name = "VIP"},
+                // };
+
+                // foreach (var role in roles)
+                // {
+                //     _roleManager.CreateAsync(role).Wait();
+                // }
+
+                foreach (var user in users)
+                {
+                    // user.Photos.SingleOrDefault().IsApproved = true;
+                    // _userManager.CreateAsync(user, "password").Wait();
+                    // _userManager.AddToRoleAsync(user, "Member").Wait();
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHash("password", out passwordHash, out passwordSalt);
+
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.UserName = user.UserName.ToLower();
+
+                    _context.Users.Add(user);
+
+                }
+                _context.SaveChanges();
+                // var adminUser = new User
+                // {
+                //     UserName = "Admin"
+                // };
+
+                // IdentityResult result = _userManager.CreateAsync(adminUser, "password").Result;
+
+                // if (result.Succeeded)
+                // {
+                //     var admin = _userManager.FindByNameAsync("Admin").Result;
+                //     _userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"}).Wait();
+                // }
+            //}
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
