df1720817c5e52b17d1c43c8609bec76b3467971
amirgc <gcamir15@gmail.com>
2019-01-11 00:48:35 -0600

    added new admin module

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,33 +1,33 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { ValueComponent } from "./value/value.component";
-import { NavComponent } from "./nav/nav.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { JwtModule } from "@auth0/angular-jwt";
-import { NgxGalleryModule } from "ngx-gallery";
-import { TimeAgoPipe } from "time-ago-pipe";
-
-import { HomeComponent } from "./home/home.component";
-import { RegisterComponent } from "./register/register.component";
 import {
   BsDropdownModule,
   TabsModule,
   BsDatepickerModule,
   PaginationModule,
-  ButtonsModule
+  ButtonsModule,
+  ModalModule
 } from "ngx-bootstrap";
 import { RouterModule } from "@angular/router";
-import { appRoutes } from "./routes";
+import { JwtModule } from "@auth0/angular-jwt";
+import { NgxGalleryModule } from "ngx-gallery";
+import { FileUploadModule } from "ng2-file-upload";
+import { TimeAgoPipe } from "time-ago-pipe";
 
-import { ErrorInterceptorProvider } from "./_services/error.interceptor";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
 import { AuthService } from "./_services/auth.service";
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
+import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 import { AlertifyService } from "./_services/alertify.service";
+import { MemberListComponent } from "./members/member-list/member-list.component";
 import { ListsComponent } from "./lists/lists.component";
 import { MessagesComponent } from "./messages/messages.component";
-import { MemberListComponent } from "./members/member-list/member-list.component";
+import { appRoutes } from "./routes";
+import { AuthGuard } from "./_guards/auth.guard";
 import { UserService } from "./_services/user.service";
 import { MemberCardComponent } from "./members/member-card/member-card.component";
 import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
@@ -37,10 +37,15 @@ import { MemberEditComponent } from "./members/member-edit/member-edit.component
 import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
 import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
 import { PhotoEditorComponent } from "./members/photo-editor/photo-editor.component";
-import { FileUploadModule } from "ng2-file-upload";
 import { ListsResolver } from "./_resolvers/lists.resolver";
-import { MessagesResolver } from "./_resolvers/message.resolver";
+import { MessagesResolver } from "./_resolvers/messages.resolver";
 import { MemberMessagesComponent } from "./members/member-messages/member-messages.component";
+import { AdminPanelComponent } from "./admin/admin-panel/admin-panel.component";
+import { HasRoleDirective } from "./_directives/hasRole.directive";
+import { UserManagementComponent } from "./admin/user-management/user-management.component";
+import { PhotoManagementComponent } from "./admin/photo-management/photo-management.component";
+import { AdminService } from "./_services/admin.service";
+import { RolesModalComponent } from "./admin/roles-modal/roles-modal.component";
 
 export function tokenGetter() {
   return localStorage.getItem("token");
@@ -49,32 +54,36 @@ export function tokenGetter() {
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent,
     NavComponent,
     HomeComponent,
     RegisterComponent,
+    MemberListComponent,
     ListsComponent,
     MessagesComponent,
-    MemberListComponent,
     MemberCardComponent,
     MemberDetailComponent,
     MemberEditComponent,
     PhotoEditorComponent,
+    TimeAgoPipe,
     MemberMessagesComponent,
-    TimeAgoPipe
+    AdminPanelComponent,
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
+    RolesModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     BsDropdownModule.forRoot(),
     BsDatepickerModule.forRoot(),
     ButtonsModule.forRoot(),
-    RouterModule.forRoot(appRoutes),
-    TabsModule.forRoot(),
     PaginationModule.forRoot(),
+    TabsModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    ModalModule.forRoot(),
     NgxGalleryModule,
     FileUploadModule,
     JwtModule.forRoot({
@@ -87,16 +96,19 @@ export function tokenGetter() {
   ],
   providers: [
     AuthService,
-    PreventUnsavedChanges,
     ErrorInterceptorProvider,
     AlertifyService,
+    AuthGuard,
     UserService,
     MemberDetailResolver,
     MemberListResolver,
     MemberEditResolver,
+    PreventUnsavedChanges,
     ListsResolver,
-    MessagesResolver
+    MessagesResolver,
+    AdminService
   ],
+  entryComponents: [RolesModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
